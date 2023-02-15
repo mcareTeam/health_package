d35eecc43d203f0fe58265f871add91ce5c1ffb6
Nguyen Xuan Thao <nxthao@tma.com.vn>
2023-02-15 15:13:26 +0700

    first commit

---
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/thaonguyen/Application/flutter_3.3.2"
+export "FLUTTER_APPLICATION_PATH=/Users/thaonguyen/Flutter/Project/Source_code/vHealth/dev/product/development/health/example"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=/Users/thaonguyen/Flutter/Project/Source_code/vHealth/dev/product/development/health/example/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=4.4.0"
+export "FLUTTER_BUILD_NUMBER=4.4.0"
+export "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/thaonguyen/Flutter/Project/Source_code/vHealth/dev/product/development/health/example/.dart_tool/package_config.json"
